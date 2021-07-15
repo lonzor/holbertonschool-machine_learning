d541d96364b25bc8ce704ef7bf434b12303dddfe
Lonzo Rust <rustlonzo@gmail.com>
2021-07-15 19:49:24 +0000

    Adds two arrays to create a single list.

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Concatenates two arrays with the result being a list"""
+
+
+def cat_arrays(arr1, arr2):
+    """Addition of both arrays"""
+    catted_arr = arr1 + arr2
+    return catted_arr
