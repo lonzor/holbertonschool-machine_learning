2d0a258765a89f3615e4a55b253eac8811a44e28
Lonzo Rust <rustlonzo@gmail.com>
2021-07-15 19:06:20 +0000

    Adds elements of 2D matrices.

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Adds the elements of two matrices at the same position"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    If mat1 and mat2 are not the same shape, returns None.
+    A new matrix is created with the elements' addition.
+    """
+    mat_sum = []
+
+    if len(mat1) != len(mat2):
+        return None
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+
+    for r in range(len(mat1)):
+        mat_sum.append([])
+        for c in range(len(mat1[0])):
+            mat_sum[r].append(mat1[c][r] + mat2[c][r])
+    return mat_sum
