0b40815464583688c1da1ce2342bbe303d735ed1
Lonzo Rust <rustlonzo@gmail.com>
2021-07-16 17:04:19 +0000

    peforms matrix multiplication via numpy

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""performs matrix multiplication with numpy"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """multiplies matrices via numpy"""
+    return np.matmul(mat1, mat2)
