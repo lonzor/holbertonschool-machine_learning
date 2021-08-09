3c1f062979a25d3d32e25f9904c850039e6181a0
Lonzo Rust <rustlonzo@gmail.com>
2021-08-09 18:21:40 +0000

    Created class for deep neural network.

---
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Contains class DeepNeuralNetwork"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    defines a deep neural network performing binary classification
+    """
+
+    def __init__(self, nx, layers):
+        """
+        constructor for DeepNeuralNetwork class
+        nx is the number of input features
+        layers is a list representing the number of nodes
+        in each layer of the network
+        L: The number of layers in the neural network.
+        cache: A dictionary to hold values of the network.
+        weights: A dictionary to hold all weights and bias
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if type(layers) is not list:
+            raise TypeError("layers must be a list of positive integers")
+        if len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        prvs = nx
+        for i in range(self.L):
+            if type(layers[i]) is not int or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            w = "W{}".format(i + 1)
+            b = "b{}".format(i + 1)
+            self.weights[w] = np.random.randn(layers[i], prvs)\
+                * np.sqrt(2 / prvs)
+            self.weights[b] = np.zeros((layers[i], 1))
+            prvs = layers[i]
