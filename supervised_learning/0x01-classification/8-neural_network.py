fe788eb60fd417811a4091d73590d0427bd3abd4
Lonzo Rust <rustlonzo@gmail.com>
2021-08-09 16:06:41 +0000

    Create class that defines a neural network with one hidden layer.

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""contains NeuralNetwork class"""
+from typing import Type
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    defines a neural network with one hidden layer
+    performing binary classification
+    """
+
+    def __init__(self, nx, nodes):
+        """
+        constructor for class NeuralNetwork
+        nx is the number of input features
+        nodes is the number of nodes found in the hidden layer
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
