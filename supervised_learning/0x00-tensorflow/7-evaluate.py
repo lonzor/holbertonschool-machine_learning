65a44b2117b0254c24c4ef30d157b78b87ad7325
Lonzo Rust <rustlonzo@gmail.com>
2021-08-17 14:49:34 +0000

    Created evaluate method using tensorflow.

---
+++ supervised_learning/0x00-tensorflow/7-evaluate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""contains the evaluate method"""
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """
+    X is a numpy.ndarray containing the input data to evaluate
+    Y is a numpy.ndarray containing the one-hot labels for X
+    save_path is the location to load the model from
+    """
+    with tf.Session() as sess:
+        saved = tf.train.import_meta_graph(save_path + ".meta")
+        saved.restore(sess, save_path)
+
+        y = tf.get_collection("y")[0]
+        x = tf.get_collection("x")[0]
+        acc = tf.get_collection("acc")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        loss = tf.get_collection("loss")[0]
+        feed_dict = {x: X, y: Y}
+        pred = sess.run(y_pred, feed_dict)
+        acc = sess.run(acc, feed_dict)
+        loss = sess.run(loss, feed_dict)
+    return pred, acc, loss
