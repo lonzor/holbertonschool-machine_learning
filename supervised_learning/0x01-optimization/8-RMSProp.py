f1615415659208da79725183df71bded51279551
Lonzo Rust <rustlonzo@gmail.com>
2021-08-23 14:53:25 +0000

    creates the training operation for a neural network in tensorflow using the RMSProp optimization algorithm.

---
+++ supervised_learning/0x01-optimization/8-RMSProp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+creates the training operation for a neural network in tensorflow
+using the RMSProp optimization algorithm
+"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """
+    loss is the loss of the network
+    alpha is the learning rate
+    beta2 is the RMSProp weight
+    epsilon is a small number to avoid division by zero
+    Returns: the RMSProp optimization operation
+    """
+    optim = tf.trainRMSPropOptimizer(alpha, decay=beta2,
+                                     epsilon=epsilon).minimize(loss)
+    return optim
