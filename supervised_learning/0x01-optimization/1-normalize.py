5803e240bfc878234077e436466d6e746b7d2fd3
Lonzo Rust <rustlonzo@gmail.com>
2021-08-23 13:40:53 +0000

    Added function that normalizes a matrix.

---
+++ supervised_learning/0x01-optimization/1-normalize.py
@@ -0,0 +1,16 @@
+#!/user/bin/env python3
+"""Standardizes a matrix"""
+import numpy as np
+
+
+def normalize(X, m, s):
+    """
+    X is the numpy.ndarray of shape (d, nx) to normalize
+    d is the number of data points
+    nx is the number of features
+    m is a numpy.ndarray of shape (nx,)
+    s is a numpy.ndarray of shape (nx,)
+    Returns: The normalized X matrix
+    """
+    norm = (X - m) / s
+    return norm
