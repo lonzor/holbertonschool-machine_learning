1aff02a8b73ac69d4b01e72184a6be8d6c3d6d59
Lonzo Rust <rustlonzo@gmail.com>
2021-08-23 15:22:37 +0000

    creates the training operation for a neural network in tensorflow using the Adam optimization algorithm.

---
+++ supervised_learning/0x01-optimization/10-Adam.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+creates the training operation for a neural network in tensorflow
+using the Adam optimization algorithm
+"""
+import tensorflow as tf
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """
+    loss is the loss of the network
+    alpha is the learning rate
+    beta1 is the weight used for the first moment
+    beta2 is the weight used for the second moment
+    epsilon is a small number to avoid division by zero
+    Returns: the Adam optimization operation
+    """
+    optim = tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon)
+    return optim.minimizer(loss)
