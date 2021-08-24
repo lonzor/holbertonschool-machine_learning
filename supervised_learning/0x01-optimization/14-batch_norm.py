be3982d664abad3eaafe97f9e20f1e8ad0e4a227
Lonzo Rust <rustlonzo@gmail.com>
2021-08-24 13:43:24 +0000

    Function that creates a batch normalization layer for a neural network in tensorflow.

---
+++ supervised_learning/0x01-optimization/14-batch_norm.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+creates a batch normalization layer for a neural network in tensorflow
+"""
+import tensorflow as tf
+
+
+def createf_batch_norm_layer(prev, n, activation):
+    """
+    prev is the activated output of the previous layer
+    n is the number of nodes in the layer to be created
+    activation is the activation function that should be used
+    - on the output of the layer
+    """
+    kern = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    base = tf.layers.Dense(n, kernel_initializer=kern)
+    m, var = tf.nn.moments(base(prev), axes=[0])
+    g = tf.Variable(tf.ones([n]), trainable=True)
+    beta = tf.Variable(tf.zeroes([n]), trainable=True)
+    epsilon = 1e-8
+
+    batch = tf.nn.batch_normalization(base(prev), m, var, beta, g, epsilon)
+    return activation(batch)
