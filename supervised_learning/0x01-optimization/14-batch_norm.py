f98b1fdfec50da2407c26140a0c93ee0457760f2
Lonzo Rust <rustlonzo@gmail.com>
2021-08-24 14:34:50 +0000

    More debugging

--- supervised_learning/0x01-optimization/14-batch_norm.py
+++ supervised_learning/0x01-optimization/14-batch_norm.py
@@ -12,12 +12,11 @@ def createf_batch_norm_layer(prev, n, activation):
     activation is the activation function that should be used
     - on the output of the layer
     """
-    kern = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    base = tf.layers.dense(prev, n, kernel_initializer=kern)
-    m, var = tf.nn.moments(base(prev), axes=[0])
-    g = tf.Variable(tf.ones(n), trainable=True)
+    w = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    base = tf.layers.dense(prev, n, kernel_initializer=w)
+    m, var = tf.nn.moments(base, 0)
     beta = tf.Variable(tf.zeros(n), trainable=True)
+    g = tf.Variable(tf.ones(n), trainable=True)
     epsilon = 1e-8
-
-    batch = tf.nn.batch_normalization(base, m, var, beta, g, epsilon)
-    return activation(batch)
+    batch_norm = tf.nn.batch_normalization(base, m, var, beta, g, epsilon)
+    return activation(batch_norm)
