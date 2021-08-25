6941ec4341a500b6500fceb291f380654c1a03a6
Lonzo Rust <rustlonzo@gmail.com>
2021-08-25 16:46:02 +0000

    Method that creates a confusion matrix.

---
+++ supervised_learning/0x00-error_analysis/0-create_confusion.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Contains method create_confusion_matrix
+"""
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """
+    labels is a one-hot numpy.ndarray of shape (m, classes)
+    - containing the correct labels for each data point
+    m is the number of data points
+    classes is the number of classes
+    logits is a one-hot numpy.ndarray of shape (m, classes)
+    - containing the predicted labels
+    Returns: a confusion numpy.ndarray of shape (classes, classes)
+    - with row indices representing the correct labels
+    - and column indices representing the predicted labels
+    """
+    confusion = np.matmul(labels.transpose(), logits)
+    return confusion
