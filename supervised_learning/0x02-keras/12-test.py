f07eb0df67377f02236ec1bb30acac11f71e491c
Lonzo Rust <rustlonzo@gmail.com>
2021-09-02 19:12:27 +0000

    function that tests a neural network.

---
+++ supervised_learning/0x02-keras/12-test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+contains function that tests a neural network
+"""
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """
+    network is the network model to test
+    data is the input data to test the model with
+    labels are the correct one-hot labels of data
+    verbose is a boolean that determines if output should be printed
+    """
+    test = network.evaluate(x=data, y=labels, verbose=verbose)
+    return test
