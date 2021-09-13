adacc803b2ecff9f73df014853212452dd386f55
Lonzo Rust <rustlonzo@gmail.com>
2021-09-13 10:44:55 +0000

    Debug Round 2.

--- supervised_learning/0x00-cnn/0-conv_forward.py
+++ supervised_learning/0x00-cnn/0-conv_forward.py
@@ -26,7 +26,7 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     result = np.zeros((m, next_h, next_w, cn))
     pad_s = ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0))
     pad_img = np.pad(A_prev, pad_width=pad_s, mode='constant',
-                    constant_values=0)
+                     constant_values=0)
 
     for i in range(next_h):
         x = i * sh
@@ -38,5 +38,6 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
             result[:, i, j, k] = np.sum(np.multiply(img, kernel),
                                         axis=(1, 2, 3))
 
-    result = result + b
+            result[:, i, j, k] = activation(
+                result[:, i, j, k] + b[:, :, :, k])
     return activation(result)
