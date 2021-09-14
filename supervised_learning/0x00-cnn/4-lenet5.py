40a3dbfd3e0a4b2196f4268493ed133e2749c169
Lonzo Rust <rustlonzo@gmail.com>
2021-09-14 23:20:10 +0000

    builds a modified version of the LeNet-5 architecture using tensorflow

---
+++ supervised_learning/0x00-cnn/4-lenet5.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Contains function lenet5
+"""
+import tensorflow as tf
+
+
+def lenet5(x, y):
+    """
+    builds a modified version of the LeNet-5 architecture using tensorflow
+    """
+    init = tf.contrib.layers.variance_scaling_initializer()
+    con_1 = tf.layers.Conv2D(filters=6, kernel_size=(5, 5), padding='same',
+                             activation='relu', kernel_initializer=init)(x)
+    pool_1 = tf.layers.MaxPooling2D(pool_size=[2, 2], strides=2)(con_1)
+    con_2 = tf.layers.Conv2D(filters=16, kernel_size=5, padding='valid',
+                             activation='relu',
+                             kernel_initializer=init)(pool_1)
+    pool_2 = tf.layers.MaxPooling2D(pool_size=[2, 2], strides=2)(con_2)
+
+    flat = tf.layers.Flatten()(pool_2)
+    c_layer1 = tf.layers.Dense(units=120, kernel_initializer=init,
+                               acitivation='relu')(flat)
+    c_layer2 = tf.layers.Dense(units=84, kernel_initializer=init,
+                               acitivation='relu')(c_layer1)
+    pred = tf.layers.Dense(units=10, kernel_initializer=init)(c_layer2)
+    result = tf.nn.softmax(pred)
+
+    loss = tf.losses.softmax_cross_entropy(y, pred)
+    adam_train = tf.train.AdamOptimizer().minimize(loss)
+    correct_pred = tf.equal(tf.argmax(y, 1), tf.argmax(pred, 1))
+    acc = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+    return result, adam_train, loss, acc
