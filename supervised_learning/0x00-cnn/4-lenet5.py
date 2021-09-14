bc8d519089c4595567a0ae07c200b1ec4bc9267e
Lonzo Rust <rustlonzo@gmail.com>
2021-09-14 23:47:01 +0000

    Debug Round 3

--- supervised_learning/0x00-cnn/4-lenet5.py
+++ supervised_learning/0x00-cnn/4-lenet5.py
@@ -10,21 +10,20 @@ def lenet5(x, y):
     builds a modified version of the LeNet-5 architecture using tensorflow
     """
     init = tf.contrib.layers.variance_scaling_initializer()
-    activation = tf.nn.relu
-    con_1 = tf.layers.Conv2D(filters=6, kernel_size=(5, 5), padding='same',
-                             activation=activation,
+    con_1 = tf.layers.Conv2D(6, kernel_size=(5, 5), padding='same',
+                             activation=tf.nn.relu,
                              kernel_initializer=init)(x)
-    pool_1 = tf.layers.MaxPooling2D(pool_size=[2, 2], strides=(2, 2))(con_1)
+    pool_1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(con_1)
     con_2 = tf.layers.Conv2D(filters=16, kernel_size=5, padding='valid',
-                             activation=activation,
+                             activation=tf.nn.relu,
                              kernel_initializer=init)(pool_1)
-    pool_2 = tf.layers.MaxPooling2D(pool_size=[2, 2], strides=(2, 2))(con_2)
+    pool_2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(con_2)
 
     flat = tf.layers.Flatten()(pool_2)
     c_layer1 = tf.layers.Dense(units=120, kernel_initializer=init,
-                               acitivation=activation)(flat)
+                               activation=tf.nn.relu)(flat)
     c_layer2 = tf.layers.Dense(units=84, kernel_initializer=init,
-                               acitivation=activation)(c_layer1)
+                               activation=tf.nn.relu)(c_layer1)
     c_layer3 = tf.layers.Dense(units=10, kernel_initializer=init)(c_layer2)
     pred = c_layer3
 
