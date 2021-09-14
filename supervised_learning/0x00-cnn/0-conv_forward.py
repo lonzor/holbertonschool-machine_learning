2761e01738fed6c72044cd6638ada8a3a06c430a
Lonzo Rust <rustlonzo@gmail.com>
2021-09-14 20:34:35 +0000

    Debug round 3.

--- supervised_learning/0x00-cnn/0-conv_forward.py
+++ supervised_learning/0x00-cnn/0-conv_forward.py
@@ -11,33 +11,30 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     - of a neural network
     """
     m, h, w, c = A_prev.shape
-    kern_h, kern_w, cn = W.shape[0], W.shape[1], W.shape[2]
+    kern_h, kern_w, c, cn = W.shape
     sh, sw = stride
-    if padding == 'same':
-        pad_h = int(((h - 1) * sh + kern_h - h) / 2)
-        pad_w = int(((w - 1) * sw + kern_w - w) / 2)
 
-    else:
+    if padding == 'same':
+        pad_h = (((h - 1) * sh) + kern_h - h) // 2
+        pad_w = (((w - 1) * sw) + kern_w - w) // 2
+
+    if padding == 'valid':
         pad_h = 0
         pad_w = 0
 
-    next_h = int(((h - kern_h + (2 * pad_h)) / sh + 1))
-    next_w = int(((w - kern_w + (2 * pad_w)) / sw + 1))
+    pad_img = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)),
+                     mode='constant', constant_values=0)
+    next_h = (h - kern_h + 2 * pad_h) // sh + 1
+    next_w = (w - kern_w + 2 * pad_w) // sw + 1
     result = np.zeros((m, next_h, next_w, cn))
-    pad_s = ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0))
-    pad_img = np.pad(A_prev, pad_width=pad_s, mode='constant',
-                     constant_values=0)
 
     for i in range(next_h):
-        x = i * sh
         for j in range(next_w):
-            y = j * sw
             for k in range(cn):
-                img = pad_img[:, x:x + kern_h, y:y + kern_w, :]
-            kernel = W[:, :, :, k]
-            result[:, i, j, k] = np.sum(np.multiply(img, kernel),
-                                        axis=(1, 2, 3))
-
-            result[:, i, j, k] = activation(
-                result[:, i, j, k] + b[:, :, :, k])
-    return activation(result)
+                result[:, i, j, k] = np.sum(
+                    pad_img[:, i * sh:(kern_h + (i * sh)),
+                            j * sw:(kern_w + (j * sw))]
+                    * W[:, :, :, k],
+                    axis=(1, 2, 3)
+                )
+    return result
