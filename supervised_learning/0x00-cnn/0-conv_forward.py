252c45a99abab2d8af645c3547ac9fda202d4a1d
Lonzo Rust <rustlonzo@gmail.com>
2021-09-14 20:58:33 +0000

    Debug Round 4

--- supervised_learning/0x00-cnn/0-conv_forward.py
+++ supervised_learning/0x00-cnn/0-conv_forward.py
@@ -11,30 +11,25 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     - of a neural network
     """
     m, h, w, c = A_prev.shape
-    kern_h, kern_w, c, cn = W.shape
+    kern_h, kern_w, kc, nc = W.shape
     sh, sw = stride
 
-    if padding == 'same':
-        pad_h = (((h - 1) * sh) + kern_h - h) // 2
-        pad_w = (((w - 1) * sw) + kern_w - w) // 2
-
     if padding == 'valid':
-        pad_h = 0
-        pad_w = 0
+        ph, pw = 0, 0
+    if padding == 'same':
+        ph = (((h - 1) * sh) + kern_h - h) // 2
+        pw = (((w - 1) * sw) + kern_w - w) // 2
 
-    pad_img = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)),
+    pad_img = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                      mode='constant', constant_values=0)
-    next_h = (h - kern_h + 2 * pad_h) // sh + 1
-    next_w = (w - kern_w + 2 * pad_w) // sw + 1
-    result = np.zeros((m, next_h, next_w, cn))
-
-    for i in range(next_h):
-        for j in range(next_w):
-            for k in range(cn):
-                result[:, i, j, k] = np.sum(
-                    pad_img[:, i * sh:(kern_h + (i * sh)),
-                            j * sw:(kern_w + (j * sw))]
-                    * W[:, :, :, k],
-                    axis=(1, 2, 3)
-                )
-    return result
+    nh = (h + (2 * ph) - kern_h) // sh + 1
+    nw = (w + (2 * pw) - kern_w) // sw + 1
+    result = np.zeros((m, nh, nw, nc))
+    for i in range(nc):
+        for j in range(nh):
+            for k in range(nw):
+                result[:, j, k, i] = np.sum(np.multiply(
+                    W[:, :, :, i],
+                    pad_img[:, sh*j: sh*j + kern_h, sw*k:sw*k + kern_h]),
+                    axis=(1, 2, 3)) + b[:, :, :, i]
+    return activation(result)
