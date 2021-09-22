d021c76d09044bddfaa9c128ddaf288ed56ab3f4
Lonzo Rust <rustlonzo@gmail.com>
2021-09-22 05:25:35 +0000

    Debug Round 1.

--- supervised_learning/0x01-deep_cnns/0-inception_block.py
+++ supervised_learning/0x01-deep_cnns/0-inception_block.py
@@ -22,7 +22,7 @@ def inception_block(A_prev, filters):
                                 activation='relu',
                                 kernel_initializer=init)(A_prev)
 
-    c_layer3 = K.layers.Conv2D(filters=F3, kernel_size=(1, 1),
+    c_layer3 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3),
                                padding='same',
                                activation='relu',
                                kernel_initializer=init)(c_layer3R)
@@ -32,15 +32,13 @@ def inception_block(A_prev, filters):
                                 activation='relu',
                                 kernel_initializer=init)(A_prev)
 
-    c_layer5 = K.layers.Conv2D(filters=F5, kernel_size=(1, 1),
+    c_layer5 = K.layers.Conv2D(filters=F5, kernel_size=(5, 5),
                                padding='same',
                                activation='relu',
                                kernel_initializer=init)(c_layer5R)
 
-    c_pooling = K.layers.Conv2D(filters=FPP, kernel_size=(1, 1),
-                                padding='same',
-                                activation='relu',
-                                kernel_initializer=init)(A_prev)
+    c_pooling = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(1, 1),
+                                      padding='same')(A_prev)
 
     c_layer_pool = K.layers.Conv2D(filters=FPP, kernel_size=(1, 1),
                                    padding='same', activation='relu',
