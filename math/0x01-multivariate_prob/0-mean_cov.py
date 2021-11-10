ecd22b4941ba60152ab4abffee3a1f875822ea0f
Lonzo Rust <rustlonzo@gmail.com>
2021-11-10 16:10:23 +0000

    Reordered variables and corrected syntax of ValueError.

--- math/0x01-multivariate_prob/0-mean_cov.py
+++ math/0x01-multivariate_prob/0-mean_cov.py
@@ -9,14 +9,15 @@ def mean_cov(X):
     """
     calculates mean and covariance
     """
-    if X.shape[0] < 2:
-        raiseValueError("X must contain multiple data points")
-    if type(X) is not np.ndarray or len(X.shape) != 2:
-        raise TypeError("X must be a 2D numpy.ndarray")
-
     data_points = X.shape[0]
     dimensions = X.shape[1]
     mean = np.mean(X, axis=0).reshape(1, dimensions)
+
+    if data_points < 2:
+        raise ValueError("X must contain multiple data points")
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+
     X = X - mean
     covar = ((np.dot(X.T, X)) / (data_points - 1))
     return mean, covar
