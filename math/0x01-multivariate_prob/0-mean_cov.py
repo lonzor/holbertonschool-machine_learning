04705c46d6a7c79bd9cb53e22e5db205993c2994
Lonzo Rust <rustlonzo@gmail.com>
2021-11-10 16:18:29 +0000

    Debug Round 3

--- math/0x01-multivariate_prob/0-mean_cov.py
+++ math/0x01-multivariate_prob/0-mean_cov.py
@@ -9,15 +9,14 @@ def mean_cov(X):
     """
     calculates mean and covariance
     """
-    data_points = X.shape[0]
     dimensions = X.shape[1]
-    mean = np.mean(X, axis=0).reshape(1, dimensions)
 
-    if data_points < 2:
+    if X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
     if type(X) is not np.ndarray or len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
 
-    X = X - mean
-    covar = ((np.dot(X.T, X)) / (data_points - 1))
+    mean = np.zeros((1, dimensions))
+    mean[0] = np.mean(X, axis=0)
+    covar = np.dot(X.T, X - mean) / (X.shape[0] - 1)
     return mean, covar
