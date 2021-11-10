098b1a85d464436a5b2ed41c5fa5d969a3b93704
Lonzo Rust <rustlonzo@gmail.com>
2021-11-10 18:25:07 +0000

    added function to calculate PDF of a data point.

--- math/0x01-multivariate_prob/multinormal.py
+++ math/0x01-multivariate_prob/multinormal.py
@@ -24,3 +24,20 @@ class MultiNormal():
         mean = np.mean(data, axis=1).reshape((data.shape[0], 1))
         self.mean = mean
         self.cov = np.matmul(data - self.mean, data.T) / (data.shape[1] - 1)
+
+    def pdf(self, x):
+        """
+        calculates the PDF at a data point
+        """
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        if (len(x.shape) != 2 or x.shape[1] != 1):
+            raise ValueError("x must have the shape ({}, 1")
+        if x.shape[0] != self.mean.shape[0]:
+            raise ValueError("x must have the shape({}, 1")
+
+        d = np.sqrt(((2 * np.pi) ** x.shape[0]) * np.linalg.det(self.cov))
+        c = np.linalg.inv(self.cov)
+        e = (-0.5 * np.matmul(np.matmul((x - self.mean).T, c), x - self.mean))
+        pdf = (1 / d) * np.exp(e[0][0])
+        return pdf
