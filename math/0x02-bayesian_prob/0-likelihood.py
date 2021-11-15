f9017c44e162f2a65e1ad7d095c283c86278adb5
Lonzo Rust <rustlonzo@gmail.com>
2021-11-15 23:25:48 +0000

    Correct minor error with raise ValueError.

--- math/0x02-bayesian_prob/0-likelihood.py
+++ math/0x02-bayesian_prob/0-likelihood.py
@@ -12,7 +12,7 @@ def likelihood(x, n, P):
     if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
     if type(x) is not int or x < 0:
-        raiseValueError(
+        raise ValueError(
             "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
