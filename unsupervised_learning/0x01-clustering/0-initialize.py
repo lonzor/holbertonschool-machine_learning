ec042768d116490e73a4ac9e56f8640398474b93
Lonzo Rust <rustlonzo@gmail.com>
2021-11-30 17:45:51 +0000

    Debug Round 1

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -9,14 +9,12 @@ def initialize(X, k):
     """
     Initializes cluster centroids for K-means
     """
-    if type(X) is not int or type(x) is not np.ndarray:
+    if not isinstance(k, int) or k <= 0:
         return None
-    if len(X.shape) != 2 or k < 0:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
 
     n, d = X.shape
-    if k == 0:
-        return None
-    min_val = np.amin(X, axis=0)
-    max_val = np.amax(X, axis=0)
-    return np.random.uniform(low, high, size=(k, d))
+    min_val = np.min(X, axis=0)
+    max_val = np.max(X, axis=0)
+    return np.random.uniform(low=min_val, high=max_val, size=(k, d))
