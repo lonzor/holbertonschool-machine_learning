4282756382fb07eee2cd720447867431015a7f1b
Lonzo Rust <rustlonzo@gmail.com>
2021-12-01 03:15:56 +0000

    Added initialize function and reworked task.

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -5,37 +5,44 @@ contains function kmeans()
 import numpy as np
 
 
-def kmeans(X, k, iterations=1000):
+def initialize(X, k):
     """
-    Performs kmeans on a dataset
+    Initializes cluster centroids for K-means
     """
-    if type(X) is not np.ndarray or len(X.shape) != 2:
-        return None, None
-    if type(k) is not int or k <= 0 or k > X.shape[0]:
-        return None, None
-    if type(iterations) is not int or iterations <= 0:
-        return None, None
+    if not isinstance(k, int) or k <= 0:
+        return None
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
 
     n, d = X.shape
     min_val = np.min(X, axis=0)
     max_val = np.max(X, axis=0)
-    centroid = np.random.uniform(low=min_val, high=max_val, size=(k, d))
+    return np.random.uniform(low=min_val, high=max_val, size=(k, d))
 
-    for i in range(iterations):
-        distan = np.sqrt(((X - centroid[:, np.newaxis])**2).sum(axis=2))
-        clss = np.argmin(distan, axis=0)
-        cent_cpy = centroid.copy()
-        for c in range(k):
-            if len(X[c == clss]) == 0:
-                centroid[c] = np.random.uniform(low=np.min(X, axis=0),
-                                                high=np.max(X, axis=0),
-                                                size=(1, d))
+
+def kmeans(X, k, iterations=1000):
+    """
+    Performs kmeans on a dataset
+    """
+    if type(iterations) is not int or iterations < 1:
+        return None, None
+    centroids = initialize(X, k)
+    if centroids is None:
+        return None, None
+    num_manip = None
+    while iterations:
+        iterations = iterations - 1
+        cent_copy = centroids.copy()
+        arr_manip = np.apply_along_axis(np.subtract, 1, X, centroids)
+        arr_manip = np.argmin(np.square(arr_manip).sum(axis=2), axis=1)
+        for c in range(centroids.shape[0]):
+            X_where = np.argwhere(arr_manip == c)
+            if X_where.shape[0] == 0:
+                centroids[c] = initialize(X, 1)
             else:
-                centroid[c] = np.mean(X[c == clss], axis=0)
-        if np.array_equal(cent_cpy, centroid):
-            break
-
-        distan = np.sqrt(((X - centroid[:, np.newaxis])**2).sum(axis=2))
-        clss = np.argmin(distan, axis=0)
-
-        return centroid, clss
+                centroids[c] = np.mean(X[X_where], axis=0)
+        if np.all(cent_copy == centroids):
+            return centroids, arr_manip
+    arr_manip = np.apply_along_axis(np.subtract, 1, X, centroids)
+    arr_manip = np.argmin(np.square(arg_manip).sum(axis=2), axis=1)
+    return centroids, arr_manip
