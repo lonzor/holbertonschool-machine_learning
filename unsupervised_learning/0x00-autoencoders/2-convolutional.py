67991064b246398dbbf97d857b92f600d42be777
Lonzo Rust <rustlonzo@gmail.com>
2022-01-04 19:43:11 +0000

    Debug Round 1.

--- unsupervised_learning/0x00-autoencoders/2-convolutional.py
+++ unsupervised_learning/0x00-autoencoders/2-convolutional.py
@@ -9,27 +9,27 @@ def autoencoder(input_dims, filters, latent_dims):
     """
     Creates autoencoder
     """
-    inputs = keras.layers.Input(shape=(input_dims,))
+    inputs = keras.layers.Input(shape=input_dims)
     encoded = inputs
 
-    for i in range(1, len(filters)):
-        encoded = keras.layers.Conv2D(filters[i], (3, 3), padding=same
+    for i in range(len(filters)):
+        encoded = keras.layers.Conv2D(filters[i], (3, 3), padding='same',
                                       activation='relu')(encoded)
 
-    encoded = keras.layers.MaxPooling2D((2, 2), padding='same')(encoded)
-    decoded = keras.layers.Input(shape=(latent_dims,))
+        encoded = keras.layers.MaxPooling2D((2, 2), padding='same')(encoded)
+    decoded = keras.layers.Input(shape=latent_dims)
     input_decoded = decoded
     padding = 'same'
 
     for i in range(len(filters) - 1, -1, -1):
         if i == 0:
             padding = 'valid'
-        decoded = keras.layers.Conv2D(filters[i], (3, 3), padding='same',
+        decoded = keras.layers.Conv2D(filters[i], (3, 3), padding=padding,
                                       activation='relu')(decoded)
         decoded = keras.layers.UpSampling2D((2, 2))(decoded)
 
     decoded = keras.layers.Conv2D(input_dims[-1], (3, 3), activation='sigmoid',
-                                  padding=padding)(decoded)
+                                  padding='same')(decoded)
     encoder = keras.models.Model(inputs, encoded)
     decoder = keras.models.Model(input_decoded, decoded)
 
