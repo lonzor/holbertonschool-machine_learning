7cb8c334a37dfd03db674fdae88882413589a882
Lonzo Rust <rustlonzo@gmail.com>
2022-02-08 22:34:04 +0000

    For some reason when I create and compile this on Xenial Ubuntu via Vagrant, it doesn't compile. I created the file in VsCode and it compiled without issue. It has something to do with how it is importing TfidVectorizer.

--- supervised_learning/0x00-word_embeddings/1-tf_idf.py
+++ supervised_learning/0x00-word_embeddings/1-tf_idf.py
@@ -2,17 +2,17 @@
 """
 Contains function tf_idf()
 """
-
-from sklearn.feature_extraction.text import TfidVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def tf_idf(sentences, vocab=None):
     """
-    Creates a TF-IDF word embedding model
+    Creates a TF-IDF embedding model
     """
-    vect = TfidVectorizer(lowercase=True, vocabulary=vocab)
-    X = vect.fit+transform(sentences)
+    vect = TfidfVectorizer(lowercase=True, vocabulary=vocab)
+    X = vect.fit_transform(sentences)
     embeddings = X.toarray()
     features = vect.get_feature_names()
 
     return embeddings, features
+    
