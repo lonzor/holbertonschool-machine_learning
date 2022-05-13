e0d1542ec56d2f4b758a2e813c64c996091d2cf3
Lonzo Rust <rustlonzo@gmail.com>
2022-05-13 19:24:30 +0000

    Debug Round 3

--- pipeline/0x00-databases/34-log_stats.py
+++ pipeline/0x00-databases/34-log_stats.py
@@ -6,13 +6,10 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    """
-    Provides stats on MongoDB
-    """
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs = client.logs.nginx
-    doc = logs.count_documents({})
-    print("{} logs".format(doc))
+    count = logs.count_documents({})
+    print("{} logs".format(count))
     print("Methods:")
     lst = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for i in lst:
