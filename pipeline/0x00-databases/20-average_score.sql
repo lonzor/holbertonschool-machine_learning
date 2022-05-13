1985269d149aa78233fc232d8c0432315036db1f
Lonzo Rust <rustlonzo@gmail.com>
2022-05-13 16:42:38 +0000

    Debug Round 3.

--- pipeline/0x00-databases/20-average_score.sql
+++ pipeline/0x00-databases/20-average_score.sql
@@ -1,10 +1,10 @@
 -- calculates the average score for a student by creating a procedure
 delimiter //
-    CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id_new INT)
+    CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
     BEGIN
-        SET @average = (SELECT AVG(corrections.score)
-        FROM corrections
-        WHERE corrections.user_id = user_id_new);
-        UPDATE users SET average_score = @average WHERE id = user_id;
+        UPDATE users
+        SET average_score = (SELECT AVG(corrections.score)
+            FROM corrections WHERE corrections.user_id = user_id)
+        WHERE id = user_id;
     END;
 delimiter;
