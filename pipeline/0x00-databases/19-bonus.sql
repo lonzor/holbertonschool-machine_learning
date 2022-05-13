7a8e018123b9ff8d8f4febd1a9556fa5b6737a7f
Lonzo Rust <rustlonzo@gmail.com>
2022-05-13 16:07:11 +0000

    Debug Round 1.

--- pipeline/0x00-databases/19-bonus.sql
+++ pipeline/0x00-databases/19-bonus.sql
@@ -1,10 +1,14 @@
 -- creates a trigger to add a bonus for a students grade
-CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
-BEGIN
-    IF NOT EXISTS (SELECT name FROM projects WHERE name=project_name) THEN
-            INSERT INTO projects(name)
-            VALUES (project_name);
+delimiter //
+CREATE PROCEDURE AddBonus(
+    IN user_id_new INT,
+    IN project_name varchar(255),
+    IN score_name INT)
+    BEGIN
+        IF NOT EXISTS (SELECT name FROM projects WHERE name=project_name) THEN
+            INSERT INTO projects(name) VALUES (project_name);
         END IF;
         INSERT INTO corrections(user_id, project_id, score)
-        VALUES(user_id, (SELECT id FROM projects WHERE name=project_name), score);
-END;
+            VALUES(user_id_new, (SELECT id FROM projects WHERE name = project_name), score_new);
+    END //
+delimiter;
