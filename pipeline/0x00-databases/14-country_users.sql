98f3659b223116c3c8dcca5df509257754b75608
Lonzo Rust <rustlonzo@gmail.com>
2022-05-13 15:22:24 +0000

    Debug Round 2.

--- pipeline/0x00-databases/14-country_users.sql
+++ pipeline/0x00-databases/14-country_users.sql
@@ -1,7 +1,8 @@
 -- creates a table "users"
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT ALL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(256),
+	name VARCHAR(255),
 	country ENUM('US', 'CO', 'TN') NOT NULL,
-	PRIMARY KEY (id));
+	PRIMARY KEY (id)
+);
