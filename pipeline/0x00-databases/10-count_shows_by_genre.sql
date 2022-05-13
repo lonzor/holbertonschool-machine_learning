41fb98a2c614959318c730092cdd03c6216e54e9
Lonzo Rust <rustlonzo@gmail.com>
2022-05-13 15:28:09 +0000

    Debug Round 1.

--- pipeline/0x00-databases/10-count_shows_by_genre.sql
+++ pipeline/0x00-databases/10-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
 -- lists all genres and a count of shows for each
 SELECT tv_genres.name AS genre, count(tv_show_genres.show_id) AS number_of_shows
-FROM tv_genres INNER JOIN tv_show_genres ON tv_genre.id = tv_show_genres.genre_id
+FROM tv_show_genres JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
