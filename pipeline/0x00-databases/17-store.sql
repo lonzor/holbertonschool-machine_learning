05c5b0a04a812ddbe7fad1be6943aae021dadaa9
Lonzo Rust <rustlonzo@gmail.com>
2022-05-13 15:56:12 +0000

    Debug Round 1.

--- pipeline/0x00-databases/17-store.sql
+++ pipeline/0x00-databases/17-store.sql
@@ -1,8 +1,8 @@
 -- creates trigger that decreases quantity of an item when it is purchased
+delimiter //
 CREATE TRIGGER decrease_items
-AFTER INSERT
-ON orders
-FOR EACH ROW
-    UPDATE decrease_items
-    SET quantity = quantity - NEW.number
-    WHERE items.name = NEW.item_name;
+    AFTER INSERT ON orders FOR EACH ROW
+BEGIN
+    update items SET quantity = quantity - new.number WHERE items.name=new.item_name;
+END //
+delimiter;
