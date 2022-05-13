ce9e251217e581b4b9ab1e947e40aaab7ba2c54e
Lonzo Rust <rustlonzo@gmail.com>
2022-05-13 19:51:00 +0000

    Debug Round 6

--- pipeline/0x00-databases/34-log_stats.py
+++ pipeline/0x00-databases/34-log_stats.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""
-Contains script that provides log information
-"""
-from pymongo import MongoClient
+"""shows stats about Nginx logs"""
 
 
 if __name__ == "__main__":
+    """
+    Script provides stats
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs = client.logs.nginx
     count = logs.count_documents({})
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for m in methods:
-        count2 = logs.count_documents({"method": m})
-        print("\tmethod {}: {}".format(m, count2))
+        meth = logs.count_documents({"method": meth})
+        print("\tmethod {}: {}".format(m, meth))
     fpath = {"method": "GET", "path": "/status"}
-    count2 = logs.count_documents(fpath)
-    print("{} status check".format(count2))
+    path2 = logs.count_documents(fpath)
+    print("{} status check".format(path2))
