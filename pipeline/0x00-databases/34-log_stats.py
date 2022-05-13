b0477ddf3103758a83c66682ef5e7ebc5837d396
Lonzo Rust <rustlonzo@gmail.com>
2022-05-13 19:02:34 +0000

    Uploading file for task 34.

---
+++ pipeline/0x00-databases/34-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Contains script that provides log information
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    """
+    Provides stats on MongoDB
+    """
+    db = MongoClient('mongodb://127.0.0.1:27017')
+    lg = client.logs.nginx
+    doc = logs.count_documents({})
+    print("{} logs".format(doc))
+    print("Methods:")
+    lst = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for i in lst:
+        method2 = lg.count_documents({"method": i})
+        print("\tmethod {}: {}".format(i, method2))
+    fpath = {"method": "GET", "path": "/status"}
+    path2 = lg.count_documents(fpath)
+    print("{} status check".format(path2))
