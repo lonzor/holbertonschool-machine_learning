bee113926764c2d07204cbe428ec2c7dd424b74a
Lonzo Rust <rustlonzo@gmail.com>
2022-05-13 19:13:45 +0000

    Debug Round 3

--- pipeline/0x00-databases/34-log_stats.py
+++ pipeline/0x00-databases/34-log_stats.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     print("Methods:")
     lst = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for i in lst:
-        method2 = logs.count_documents({"method": i})
-        print("\tmethod {}: {}".format(i, method2))
+        count2 = logs.count_documents({"method": i})
+        print("\tmethod {}: {}".format(i, count2))
     fpath = {"method": "GET", "path": "/status"}
-    path2 = logs.count_documents(fpath)
-    print("{} status check".format(path2))
+    count2 = logs.count_documents(fpath)
+    print("{} status check".format(count2))
